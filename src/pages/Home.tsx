d4404328ac79bcecbd5d32f0ffffd8c6894a36df
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-03-31 11:38:18 +0100

    add title to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from 'react';
+
 const Home = () => {
+  useEffect(() => {
+    document.title = 'StreamSakura | watch anime online';
+  }, []);
+
   return <p className='text-6xl font-bold'>Home page</p>;
 };
 
