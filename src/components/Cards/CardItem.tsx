cd4e385fe18e740fd62fd82f6ce7d9d08957f6cf
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-05-03 12:22:01 +0100

    feat(cards): add details to cards and hover effects to cards title
    add type, releaseDate, totalEpisodes and ratings to cards
    add smooth color transition on cards title when being hovered

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -1,7 +1,8 @@
 import { FC, useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { Anime } from '../../index';
-import { FaPlay } from 'react-icons/fa';
+import { FaCalendarAlt, FaPlay, FaStar } from 'react-icons/fa';
+import { TbCards } from 'react-icons/tb';
 
 export const CardItem: FC<{ anime: Anime }> = ({ anime }) => {
   const imageSrc = anime.image || '';
@@ -13,7 +14,7 @@ export const CardItem: FC<{ anime: Anime }> = ({ anime }) => {
         to={`/watch/${anime.id}`}
         title={anime.title.english || anime.title.romaji}
       >
-        <div className='w-full rounded-lg transition duration-200 ease-in-out scale-100 cursor-pointer'>
+        <div className='w-full rounded-lg transition duration-200 ease-in-out scale-100 cursor-pointer group/title'>
           <div className='transition duration-200 ease-in-out max-sm:hover:translate-y-2.5 max-sm:active:translate-y-2.5 max-sm:focus:translate-y-2.5'>
             <div className='rounded-lg overflow-hidden relative aspect-[3/4] shadow-blue-900 animate-slideUp transition duration-200 ease-in-out shadow-[2px_2px_10px] text-left'>
               <div className='whfull group'>
@@ -31,7 +32,7 @@ export const CardItem: FC<{ anime: Anime }> = ({ anime }) => {
           </div>
           <div className='p-2'>
             <h5
-              className='text-sm font-bold text-long'
+              className='text-sm font-bold transition duration-300 ease-in-out text-long group-hover/title:text-fuchsia-500'
               title={'Title: ' + (anime.title.english || anime.title.romaji)}
             >
               {anime.title.english || anime.title.romaji}
@@ -39,7 +40,31 @@ export const CardItem: FC<{ anime: Anime }> = ({ anime }) => {
           </div>
           <div>
             <div></div>
-            <div></div>
+            <div className='flex justify-evenly pt-1'>
+              {anime.type && (
+                <p className='px-1 text-xs font-bold rounded-sm bg-neutral-200 text-neutral-500'>
+                  {anime.type}
+                </p>
+              )}
+              {anime.releaseDate && (
+                <p className='flex items-center px-1 text-xs rounded-sm border-none bg-neutral-200 text-neutral-500'>
+                  <FaCalendarAlt />
+                  {anime.releaseDate}
+                </p>
+              )}
+              {(anime.totalEpisodes || anime.episodes) && (
+                <p className='flex items-center px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
+                  <TbCards />
+                  {anime.totalEpisodes || anime.episodes}
+                </p>
+              )}
+              {anime.rating && (
+                <p className='flex items-center px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
+                  <FaStar />
+                  {anime.rating}
+                </p>
+              )}
+            </div>
           </div>
         </div>
       </Link>
