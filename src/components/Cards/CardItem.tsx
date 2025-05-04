4f9d99ea8a2818663783ba7b4e41cc36eca4b407
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-05-04 11:58:03 +0100

    feat(cards): remove some icons and totalEpisodes, add conditional rendering to icons

--- src/components/Cards/CardItem.tsx
+++ src/components/Cards/CardItem.tsx
@@ -1,8 +1,11 @@
 import { FC, useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { Anime } from '../../index';
-import { FaCalendarAlt, FaPlay, FaStar } from 'react-icons/fa';
-import { TbCards } from 'react-icons/tb';
+import {
+  TbStarFilled,
+  TbStarHalfFilled,
+  TbPlayerPlayFilled,
+} from 'react-icons/tb';
 
 export const CardItem: FC<{ anime: Anime }> = ({ anime }) => {
   const imageSrc = anime.image || '';
@@ -23,7 +26,7 @@ export const CardItem: FC<{ anime: Anime }> = ({ anime }) => {
                   alt=''
                   className='whfull animation1 group-hover:brightness-60'
                 />
-                <FaPlay
+                <TbPlayerPlayFilled
                   className='absolute inset-0 top-1/2 left-1/2 z-10 text-3xl text-white opacity-0 transition-opacity duration-300 ease-in -translate-x-1/2 -translate-y-1/2 group-hover:opacity-100'
                   title={'Play ' + (anime.title.english || anime.title.romaji)}
                 />
@@ -42,25 +45,18 @@ export const CardItem: FC<{ anime: Anime }> = ({ anime }) => {
             <div></div>
             <div className='flex justify-evenly mt-0.5'>
               {anime.type && (
-                <p className='px-1 text-xs font-bold rounded-sm bg-neutral-200 text-neutral-500'>
+                <p className='px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
                   {anime.type}
                 </p>
               )}
               {anime.releaseDate && (
-                <p className='flex items-center px-1 text-xs rounded-sm border-none bg-neutral-200 text-neutral-500'>
-                  <FaCalendarAlt />
+                <p className='px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
                   {anime.releaseDate}
-                </p>
-              )}
-              {(anime.totalEpisodes || anime.episodes) && (
-                <p className='flex items-center px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
-                  <TbCards />
-                  {anime.totalEpisodes || anime.episodes}
                 </p>
               )}
               {anime.rating && (
                 <p className='flex items-center px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
-                  <FaStar />
+                  {anime.rating >= 75 ? <TbStarFilled /> : <TbStarHalfFilled />}
                   {anime.rating}
                 </p>
               )}
