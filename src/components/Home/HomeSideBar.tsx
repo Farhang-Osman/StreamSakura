d16d879e1fcf94e83a1237d1387ca29f6c26c821
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-05-06 19:27:02 +0100

    style(sidebar): adjust the layout more and improve the visual design

--- src/components/Home/HomeSideBar.tsx
+++ src/components/Home/HomeSideBar.tsx
@@ -10,43 +10,45 @@ export const HomeSideBar: FC<{ animeData: Anime[] }> = ({ animeData }) => {
         <Link className='' to={`/watch/${anime.id}`} key={anime.id}>
           <div className='relative aspect-[5/1]'>
             <img
-              className='object-cover absolute w-full h-full rounded-lg pl-50'
+              className='object-fill absolute right-0 w-5/6 h-full rounded-tr-lg rounded-br-lg blur-xs'
               src={anime.cover}
               alt=''
             />
-            <div className='flex relative z-10 w-full h-full rounded-lg'>
+            <div className='flex overflow-hidden relative h-full rounded-lg'>
               <img className='rounded-lg' src={anime.image} alt='' />
-              <div className='flex flex-col gap-1 ml-2'>
-                <h5 className=''>
-                  {anime.title.english || anime.title.romaji}
-                </h5>
-                <div className='flex gap-2 mt-0.5'>
-                  {anime.type && (
-                    <p className='px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
-                      {anime.type}
-                    </p>
-                  )}
-                  {anime.releaseDate && (
-                    <p className='px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
-                      {anime.releaseDate}
-                    </p>
-                  )}
-                  {anime.rating && (
-                    <p className='flex items-center px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
-                      {anime.rating >= 75 ? (
-                        <TbStarFilled />
-                      ) : (
-                        <TbStarHalfFilled />
-                      )}
-                      {anime.rating}
-                    </p>
-                  )}
+              <div className='flex flex-col justify-between ml-2'>
+                <div className='flex flex-col justify-between py-1 h-full'>
+                  <h5 className='px-1 rounded-lg bg-neutral-200 w-fit'>
+                    {anime.title.english || anime.title.romaji}
+                  </h5>
+                  <div className='flex gap-1'>
+                    {anime.type && (
+                      <p className='px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
+                        {anime.type}
+                      </p>
+                    )}
+                    {anime.releaseDate && (
+                      <p className='px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
+                        {anime.releaseDate}
+                      </p>
+                    )}
+                    {anime.rating && (
+                      <p className='flex items-center px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
+                        {anime.rating >= 75 ? (
+                          <TbStarFilled />
+                        ) : (
+                          <TbStarHalfFilled />
+                        )}
+                        {anime.rating}
+                      </p>
+                    )}
+                  </div>
                 </div>
-                <div className='flex bottom-0 gap-2'>
+                <div className='flex gap-1 text-nowrap'>
                   {anime.genres && (
                     <>
                       {anime.genres.map((item) => (
-                        <p className='px-1 text-xs rounded-sm bg-neutral-200 text-neutral-500'>
+                        <p className='px-1 text-xs rounded-md rounded-br-none rounded-bl-none bg-neutral-200 text-neutral-500'>
                           {item}
                         </p>
                       ))}
