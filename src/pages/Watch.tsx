8d5eb6858d7c81638c7a7ac2d7e061366f5a109d
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-06-30 15:51:47 +0100

    style(watch): Update UI layout

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -241,86 +241,84 @@ const Watch: FC = () => {
       <p className='w-full text-3xl text-center'>episodesLoading...</p>
     </>
   ) : (
-    <div className='grid gap-3'>
-      <div className='flex gap-3 p-1 max-xl:flex-col'>
-        <div className='relative mb-1 w-full aspect-video'>
-          {isStreamLoading && (
-            <div className='flex absolute inset-0 justify-center items-center bg-black rounded-sm'>
-              <div className='w-12 h-12 rounded-full border-t-2 border-b-2 border-blue-500 animate-spin'></div>
-            </div>
-          )}
-          <iframe
-            src={`${STREAMING_URL}/stream/s-2/${selectedEpisode}/${isSubOrDub}`}
-            className={`w-full h-full rounded-sm ${isStreamLoading ? 'opacity-0' : 'opacity-100'}`}
-            allowFullScreen
-            onLoad={handleLoad}
-          ></iframe>
+    <div className='grid grid-cols-10 gap-3 w-full'>
+      <div className='relative col-span-7 mb-1 aspect-video max-xl:col-span-10'>
+        {isStreamLoading && (
+          <div className='flex absolute inset-0 justify-center items-center bg-black rounded-sm'>
+            <div className='w-12 h-12 rounded-full border-t-2 border-b-2 border-blue-500 animate-spin'></div>
+          </div>
+        )}
+        <iframe
+          src={`${STREAMING_URL}/stream/s-2/${selectedEpisode}/${isSubOrDub}`}
+          className={`w-full h-full rounded-sm ${isStreamLoading ? 'opacity-0' : 'opacity-100'}`}
+          allowFullScreen
+          onLoad={handleLoad}
+        ></iframe>
+      </div>
+      <div className='grid col-span-3 gap-1 content-start p-1 w-full h-full bg-gray-300 rounded-sm max-xl:col-span-10 max-xl:h-72 aspect-square'>
+        <div className='px-1 text-gray-500 bg-gray-200 rounded-sm'>
+          {`Episodes ${episodes[0].episode_no} - ${episodes[episodes.length - 1].episode_no}`}
         </div>
-        <div className='grid gap-1 content-start p-1 bg-gray-300 rounded-sm min-xl:max-w-3/10 min-xl:min-w-1/4 max-xl:h-72 aspect-square'>
-          <div className='px-1 text-gray-500 bg-gray-200 rounded-sm'>
-            {`Episodes ${episodes[0].episode_no} - ${episodes[episodes.length - 1].episode_no}`}
-          </div>
-          <div
-            ref={containerRef}
-            className='grid overflow-auto gap-1 p-1 px-3 scroll-smooth max-xl:gap-y-3 max-md:gap-1 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1'
-          >
-            {episodes.map(({ ...ep }) =>
-              selectedEpisode === Number(ep.id.split('ep=')[1]) ? (
-                <button className='flex gap-4 items-center px-1 text-blue-500 bg-blue-100 rounded-sm border-2 border-blue-400 transition duration-100 ease-in-out cursor-pointer snap-center scale-103'>
-                  <FaPlay />
-                  <p>{ep.title}</p>
-                </button>
-              ) : (
-                <button
-                  onClick={(e) => onClickEp(ep, e.currentTarget)}
-                  className='flex gap-4 px-1 text-gray-500 bg-gray-100 rounded-sm transition duration-100 ease-in-out cursor-pointer hover:bg-blue-200 hover:scale-103'
-                >
-                  <p>{ep.episode_no}.</p>
-                  <p>{ep.title}</p>
-                </button>
-              ),
-            )}
-          </div>
+        <div
+          ref={containerRef}
+          className='grid overflow-auto gap-1 p-1 px-3 scroll-smooth max-xl:gap-y-3 max-md:gap-1 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1'
+        >
+          {episodes.map(({ ...ep }) =>
+            selectedEpisode === Number(ep.id.split('ep=')[1]) ? (
+              <button className='flex gap-4 items-center px-1 text-blue-500 bg-blue-100 rounded-sm border-2 border-blue-400 transition duration-100 ease-in-out cursor-pointer snap-center scale-103'>
+                <FaPlay />
+                <p>{ep.title}</p>
+              </button>
+            ) : (
+              <button
+                onClick={(e) => onClickEp(ep, e.currentTarget)}
+                className='flex gap-4 px-1 text-gray-500 bg-gray-100 rounded-sm transition duration-100 ease-in-out cursor-pointer hover:bg-blue-200 hover:scale-103'
+              >
+                <p>{ep.episode_no}.</p>
+                <p>{ep.title}</p>
+              </button>
+            ),
+          )}
         </div>
       </div>
+
       {/* beyond watch and episodes list container */}
-      <div className='flex gap-3 max-xl:flex-col'>
-        {/* Source and anime info */}
-        <div className='flex flex-col gap-3 w-full aspect-video'>
-          <div className='flex justify-evenly items-center p-2 bg-gray-300 rounded-sm'>
-            <p className='flex gap-1 p-1 px-4 bg-gray-100 rounded-sm'>
-              you are watching Episode{' '}
-              <p className='px-1 text-blue-500 bg-blue-100 rounded-sm border'>
-                {episode?.episode_no}
-              </p>
+
+      {/* Source and anime info */}
+      <div className='flex flex-col col-span-7 gap-3 w-full max-xl:col-span-10 aspect-video'>
+        <div className='flex justify-evenly items-center p-2 bg-gray-300 rounded-sm'>
+          <p className='flex gap-1 p-1 px-4 bg-gray-100 rounded-sm'>
+            you are watching Episode{' '}
+            <p className='px-1 text-blue-500 bg-blue-100 rounded-sm border'>
+              {episode?.episode_no}
             </p>
-            <div className='flex gap-4'>
-              <button
-                onClick={() => setIsSubOrDub('sub')}
-                className={`flex gap-2 items-center p-1 px-4 rounded-sm ${isSubOrDub === 'sub' ? 'bg-blue-200 text-blue-500 border-2' : 'cursor-pointer bg-gray-100 hover:bg-blue-200'}`}
-              >
-                <FaClosedCaptioning /> sub
-              </button>
-              <button
-                onClick={() => setIsSubOrDub('dub')}
-                className={`flex gap-2 items-center p-1 px-4 rounded-sm ${isSubOrDub === 'dub' ? 'bg-blue-200 text-blue-500 border-2' : 'cursor-pointer bg-gray-100 hover:bg-blue-200'}`}
-              >
-                <FaMicrophone /> dub
-              </button>
-            </div>
-          </div>
-          <div className='border'>
-            <>{animeInfo?.data.id}</>
+          </p>
+          <div className='flex gap-4'>
+            <button
+              onClick={() => setIsSubOrDub('sub')}
+              className={`flex gap-2 items-center p-1 px-4 rounded-sm ${isSubOrDub === 'sub' ? 'bg-blue-200 text-blue-500 border-2' : 'cursor-pointer bg-gray-100 hover:bg-blue-200'}`}
+            >
+              <FaClosedCaptioning /> sub
+            </button>
+            <button
+              onClick={() => setIsSubOrDub('dub')}
+              className={`flex gap-2 items-center p-1 px-4 rounded-sm ${isSubOrDub === 'dub' ? 'bg-blue-200 text-blue-500 border-2' : 'cursor-pointer bg-gray-100 hover:bg-blue-200'}`}
+            >
+              <FaMicrophone /> dub
+            </button>
           </div>
         </div>
-        {/* related and recommendations */}
-        <div className='grid grid-cols-1 gap-10 border min-xl:max-w-3/10 min-xl:min-w-1/4 max-lg:grid-cols-2 max-md:grid-cols-1 h-fit'>
-          <SideBar animeData={animeInfo.data?.related_data} title={'RELATED'} />
-          <SideBar
-            animeData={animeInfo.data?.recommended_data}
-            title={'RECOMMENDED'}
-          />
+        <div className='border'>
+          <>{animeInfo?.data.id}</>
         </div>
+      </div>
+      {/* related and recommendations */}
+      <div className='grid grid-cols-1 col-span-3 gap-10 max-xl:gap-4 max-md:gap-10 max-xl:col-span-10 max-xl:grid-cols-2 max-md:grid-cols-1 h-fit'>
+        <SideBar animeData={animeInfo.data?.related_data} title={'RELATED'} />
+        <SideBar
+          animeData={animeInfo.data?.recommended_data}
+          title={'RECOMMENDED'}
+        />
       </div>
     </div>
   );
