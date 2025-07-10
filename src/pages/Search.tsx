dd494cf5c2f983eedaee407f7aab8b4f16dae04b
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-07-10 08:44:39 +0100

    feat(search): implement the backend logic of genres, update filter interface

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -19,22 +19,30 @@ interface searchResults {
 }
 
 interface filters {
-  type: string;
-  status: string;
-  rated: string;
-  score: string;
-  season: string;
-  language: string;
-  genres: string;
-  sort: string;
-  page: number;
-  sy: number;
-  sm: number;
-  sd: number;
-  ey: number;
-  em: number;
-  ed: number;
-  keyword: string;
+  type?: 'all' | 'movie' | 'tv' | 'ova' | 'ona' | 'special' | 'music';
+  status?: 'all' | 'finished_airing' | 'currently_airing' | 'not_yet_aired';
+  rated?: 'all' | 'g' | 'pg' | 'pg_13' | 'r' | 'r_plus' | 'rx';
+  score?: 'all' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+  season?: 'all' | 'spring' | 'summer' | 'fall' | 'winter';
+  language?: 'all' | 'sub' | 'dub' | 'sub_dub';
+  genres?: string;
+  sort?:
+    | 'default'
+    | 'name_az'
+    | 'recently_added'
+    | 'recently_updated'
+    | 'score'
+    | 'name_az'
+    | 'released_date'
+    | 'most_watched';
+  page?: number;
+  sy?: number;
+  sm?: number;
+  sd?: number;
+  ey?: number;
+  em?: number;
+  ed?: number;
+  keyword?: string;
 }
 
 const genres = {
@@ -89,6 +97,28 @@ const Search: FC = () => {
   const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
   const [resp, setResp] = useState<searchResults>();
+  const [filterParams, setFilterParams] = useState<filters>();
+  const [selectedgGenres, setSelectedgGenres] = useState<string>();
+
+  async function fetchFilterResults(filterParams: filters) {
+    try {
+      const res = await axios.get(`${BASE_URL}/api/filter`, {
+        params: filterParams,
+      });
+
+      console.log(res.data.results);
+
+      if (res.status !== 200 || res.status >= 400) {
+        throw new Error(`Server Error: ${res.status}`);
+      } else if (Object.keys(res.data.results).length === 0) {
+        navigate('/404');
+      }
+
+      return res.data.results;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   async function fetchSearchResults(query: string) {
     try {
@@ -123,6 +153,31 @@ const Search: FC = () => {
     };
     fetchResults();
   }, [location]);
+
+  const handleGenres = (genreNumber: string) => {
+    if (selectedgGenres === undefined) {
+      setSelectedgGenres(genreNumber);
+    } else {
+      const genresList: string[] = selectedgGenres.split(',');
+      const genreIndex = genresList.indexOf(genreNumber);
+
+      if (genreIndex !== -1) {
+        genresList.splice(genreIndex, 1);
+        console.log('genresList.splice(genreIndex, 1)');
+        setSelectedgGenres(genresList.join(','));
+      } else {
+        genresList.push(genreNumber);
+        console.log('>>>  genresList.push(genreNumber)  <<<');
+        setSelectedgGenres(genresList.join(','));
+      }
+    }
+    // setSelectedgGenres(selectedgGenres ? [selectedgGenres, g].join(',') : g);
+  };
+  useEffect(() => {
+    console.log('genres', selectedgGenres);
+  }, [selectedgGenres]);
+
+  // console.log('genres ', selectedgGenres);
 
   return (
     <div className='flex'>
@@ -184,13 +239,17 @@ const Search: FC = () => {
       <div className='flex flex-col gap-2 p-1 w-1/5 h-full bg-gray-300 rounded-sm'>
         <h3 className='text-center label'>Genres</h3>
 
-        <div className='flex flex-wrap gap-2 justify-center'>
+        <ul className='flex flex-wrap gap-2 justify-center'>
           {Object.entries(genres).map(([id, name]) => (
-            <p key={id} className='px-1 bg-gray-200 rounded-sm border w-fit'>
+            <li
+              onClick={() => handleGenres(id)}
+              key={id}
+              className='px-1 hover:cursor-pointer bg-gray-200 rounded-sm border w-fit'
+            >
               {name}
-            </p>
+            </li>
           ))}
-        </div>
+        </ul>
       </div>
     </div>
   );
