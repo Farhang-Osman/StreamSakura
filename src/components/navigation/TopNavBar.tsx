0ee089847854946432797a35b7b5ceae83d362d6
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-07-13 15:18:37 +0100

    style(navigation): add icons & logo, remove profile

--- src/components/navigation/TopNavBar.tsx
+++ src/components/navigation/TopNavBar.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
+import { BiSolidCameraMovie } from 'react-icons/bi';
+import { FaDiscord } from 'react-icons/fa';
+import { GiPerspectiveDiceSixFacesRandom } from 'react-icons/gi';
 import { ImSearch } from 'react-icons/im';
+import { RiVipDiamondFill } from 'react-icons/ri';
 import { useNavigate } from 'react-router-dom';
+
+import blossomSvg from '/cherry-blossom-svgrepo-com-optimized.svg';
 
 const TopNavBar = () => {
   const navigate = useNavigate();
@@ -20,14 +26,23 @@ const TopNavBar = () => {
   };
 
   return (
-    <div className='flex fixed top-0 right-0 bottom-0 left-0 justify-between w-full h-16 bg-gray-200'>
-      <div className=''>logo</div>
-      <div className='flex justify-center items-center'>
+    <div className='flex fixed top-0 right-0 bottom-0 left-0 justify-around w-full h-16 bg-gray-200'>
+      <div className='flex gap-2 items-center h-full'>
+        <div className='flex items-center bg-[#bf0477] px-4 rounded-sm'>
+          <div className='h-full text-xl'>
+            <p className='text-[#ffbcd8]'>stream</p>
+            <p className='text-[#fdd0b1]'>sakura</p>
+          </div>
+          <img src={blossomSvg} className='size-15' />
+        </div>
+        <p>theme</p>
+      </div>
+      <div className='flex justify-center justify-self-center items-center'>
         <search className='flex justify-center items-center'>
           <input
             type='text'
             placeholder='search anime'
-            className='border p-1 px-2 rounded-sm'
+            className='p-1 px-2 rounded-sm border'
             value={search}
             onChange={handleInputChange}
             // onClick={() => navigate(`/search/${search}`)}
@@ -36,7 +51,24 @@ const TopNavBar = () => {
           <ImSearch />
         </search>
       </div>
-      <div>profile</div>
+      <div className='flex gap-8 items-center'>
+        <div className='flex flex-col items-center hover:cursor-pointer'>
+          <GiPerspectiveDiceSixFacesRandom className='text-blue-500 size-9' />
+          <h3 className='text-sm'>Random</h3>
+        </div>
+        <div className='flex flex-col items-center hover:cursor-pointer'>
+          <RiVipDiamondFill className='text-blue-500 size-9' />
+          <p>Special</p>
+        </div>
+        <div className='flex flex-col items-center hover:cursor-pointer'>
+          <BiSolidCameraMovie className='text-blue-500 size-9' />
+          <p>Movie</p>
+        </div>
+        <div className='flex flex-col items-center hover:cursor-pointer'>
+          <FaDiscord className='text-blue-500 size-9' />
+          <p>Dicord</p>
+        </div>
+      </div>
     </div>
   );
 };
