1e83f96f613ebba42b0579e6576ad1e97c89d2af
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-07-15 20:20:24 +0100

    feat(home): fetch data & modify HomeCarousel

--- src/components/Home/HomeCarousel.tsx
+++ src/components/Home/HomeCarousel.tsx
@@ -1,20 +1,36 @@
 import { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-import { Anime } from '../../hooks/animeInterface';
+// import { Anime } from '../../hooks/animeInterface';
 import { TbCards } from 'react-icons/tb';
 import { FaPlay, FaStar } from 'react-icons/fa';
 import { FaClock } from 'react-icons/fa6';
 import { useNavigate } from 'react-router-dom';
 
 interface HomeCarouselProps {
-  data: Anime[];
-  loading: boolean;
-  error: string | null;
+  id: string;
+  title?: string;
+  japanese_title?: string;
+  cover: string;
+  description?: string;
+  rating?: string;
+  totalEpisodes?: string;
+  duration?: string;
+  type?: string;
+  loading?: boolean;
+  error?: string | null;
 }
 
 export const HomeCarousel: FC<HomeCarouselProps> = ({
-  data = [],
+  id,
+  title,
+  japanese_title,
+  cover,
+  description,
+  rating,
+  totalEpisodes,
+  duration,
+  type,
   // loading,
   // error,
 }) => {
@@ -24,16 +40,16 @@ export const HomeCarousel: FC<HomeCarouselProps> = ({
     return navigate(`/watch/${id}`);
   };
 
-  const filteredData = data.filter(
-    (item) =>
-      item.title &&
-      item.description &&
-      item.cover !== item.image &&
-      !item.genres.includes('Ecchi') &&
-      !item.genres.includes('Hentai') &&
-      !item.genres.includes('Romance') &&
-      !item.genres.includes('Fantasy'),
-  );
+  // const filteredData = data.filter(
+  //   (item) =>
+  //     item.title &&
+  //     item.description &&
+  //     item.cover !== item.image &&
+  //     !item.genres.includes('Ecchi') &&
+  //     !item.genres.includes('Hentai') &&
+  //     !item.genres.includes('Romance') &&
+  //     !item.genres.includes('Fantasy'),
+  // );
 
   return (
     <>
@@ -69,79 +85,67 @@ export const HomeCarousel: FC<HomeCarouselProps> = ({
           keyboard={true}
           centeredSlides={true}
         >
-          {filteredData.map(
-            ({
-              id,
-              title,
-              cover,
-              description,
-              rating,
-              totalEpisodes,
-              duration,
-              type,
-            }) => (
-              <SwiperSlide
-                className='flex relative justify-start items-center'
-                key={id}
-                title={title.english || title.romaji}
-              >
-                <div className='relative w-full h-full'>
-                  <img
-                    className='object-cover absolute w-full h-full'
-                    src={cover}
-                    alt={title.english || title.romaji + ' Banner Image '}
-                  />
-                  <div className='flex flex-col justify-between h-full'>
-                    <div className='absolute bottom-6 left-8 max-lg:left-4 max-lg:bottom-6 z-5 max-w-1/2'>
-                      <h2 className='overflow-hidden m-auto max-w-full font-bold overflow-ellipsis animate-rainbow text-clamp-title min-sm:whitespace-nowrap'>
-                        {title.english}
-                      </h2>
-                      <div className='flex overflow-hidden gap-4 m-auto mt-0 text-sm text-white overflow-ellipsis max-lg:text-sm max-sm:gap-1.5'>
-                        {type && (
-                          <p className='flex px-1 text-xs font-bold text-blue-800 bg-gray-200 rounded-sm'>
-                            {type}
-                          </p>
-                        )}
-                        {totalEpisodes && (
-                          <p className='flex gap-1 items-center px-1 text-xs text-blue-800 bg-gray-200 rounded-sm'>
-                            <TbCards />
-                            {totalEpisodes}
-                          </p>
-                        )}
-                        {rating && (
-                          <p className='flex gap-1 items-center px-1 text-xs text-blue-800 bg-gray-200 rounded-sm'>
-                            <FaStar />
-                            {rating}
-                          </p>
-                        )}
-                        {duration && (
-                          <p className='flex gap-1 items-center px-1 text-xs text-blue-800 bg-gray-200 rounded-sm'>
-                            <FaClock />
-                            {duration}mins
-                          </p>
-                        )}
-                      </div>
-                      <p
-                        className='overflow-hidden overflow-y-auto max-h-16 text-xs text-blue-600 max-w-2/3'
-                        dangerouslySetInnerHTML={{ __html: description }}
-                      />
-                    </div>
-                    <div className='absolute bottom-6 right-8 z-5 max-lg:right-6'>
-                      <button
-                        className='flex p-6 bg-gray-200 rounded-full border-none transition-all duration-200 ease-in-out cursor-pointer max-sm:p-4 hover:bg-blue-200 active:bg-blue-200 focus:bg-blue-200 hover:scale-115 active:scale-115 focus:scale-115'
-                        onClick={() => handlePlayButtonCllick(id)}
-                        title={'Watch ' + (title.english || title.romaji)}
-                      >
-                        <FaPlay className='text-2xl text-blue-500' />
-                      </button>
-                    </div>
+          <SwiperSlide
+            className='flex relative justify-start items-center'
+            key={id}
+            title={title || japanese_title}
+          >
+            <div className='relative w-full h-full'>
+              <img
+                className='object-cover absolute w-full h-full'
+                src={cover}
+                alt={title || japanese_title + ' Banner Image '}
+              />
+              <div className='flex flex-col justify-between h-full'>
+                <div className='absolute bottom-6 left-8 max-lg:left-4 max-lg:bottom-6 z-5 max-w-1/2'>
+                  <h2 className='overflow-hidden m-auto max-w-full font-bold overflow-ellipsis animate-rainbow text-clamp-title min-sm:whitespace-nowrap'>
+                    {title || japanese_title}
+                  </h2>
+                  <div className='flex overflow-hidden gap-4 m-auto mt-0 text-sm text-white overflow-ellipsis max-lg:text-sm max-sm:gap-1.5'>
+                    {type && (
+                      <p className='flex px-1 text-xs font-bold text-blue-800 bg-gray-200 rounded-sm'>
+                        {type}
+                      </p>
+                    )}
+                    {totalEpisodes && (
+                      <p className='flex gap-1 items-center px-1 text-xs text-blue-800 bg-gray-200 rounded-sm'>
+                        <TbCards />
+                        {totalEpisodes}
+                      </p>
+                    )}
+                    {rating && (
+                      <p className='flex gap-1 items-center px-1 text-xs text-blue-800 bg-gray-200 rounded-sm'>
+                        <FaStar />
+                        {rating}
+                      </p>
+                    )}
+                    {duration && (
+                      <p className='flex gap-1 items-center px-1 text-xs text-blue-800 bg-gray-200 rounded-sm'>
+                        <FaClock />
+                        {duration}mins
+                      </p>
+                    )}
                   </div>
-                  <div className='absolute inset-0 bg-gradient-to-b from-transparent to-white  from-50% rounded-lg z-1'></div>
-                  <div className='absolute inset-0 bg-gradient-to-bl from-transparent to-white from-50% z-1'></div>
+                  <p
+                    className='overflow-hidden overflow-y-auto max-h-16 text-xs text-blue-600 max-w-2/3'
+                    dangerouslySetInnerHTML={{ __html: description as string }}
+                  />
                 </div>
-              </SwiperSlide>
-            ),
-          )}
+                <div className='absolute bottom-6 right-8 z-5 max-lg:right-6'>
+                  <button
+                    className='flex p-6 bg-gray-200 rounded-full border-none transition-all duration-200 ease-in-out cursor-pointer max-sm:p-4 hover:bg-blue-200 active:bg-blue-200 focus:bg-blue-200 hover:scale-115 active:scale-115 focus:scale-115'
+                    onClick={() => handlePlayButtonCllick(id)}
+                    title={'Watch ' + (title || japanese_title)}
+                  >
+                    <FaPlay className='text-2xl text-blue-500' />
+                  </button>
+                </div>
+              </div>
+              <div className='absolute inset-0 bg-gradient-to-b from-transparent to-white  from-50% rounded-lg z-1'></div>
+              <div className='absolute inset-0 bg-gradient-to-bl from-transparent to-white from-50% z-1'></div>
+            </div>
+          </SwiperSlide>
+
           <div className='swiper-pagination'></div>
         </Swiper>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,135 +1,336 @@
 import { useEffect, useState } from 'react';
-import { GiTv } from 'react-icons/gi';
-import { GrAnnounce } from 'react-icons/gr';
-import { getNextSeason } from '../hooks/useTime';
-import {
-  fetchPopularAnime,
-  fetchTopAiringAnime,
-  fetchTopAnime,
-  fetchTrendingAnime,
-  fetchUpcomingSeason,
-} from '../hooks/useApi';
-import { CardGrid } from '../components/Cards/CardGrid';
+// import { GiTv } from 'react-icons/gi';
+// import { GrAnnounce } from 'react-icons/gr';
+// import { getNextSeason } from '../hooks/useTime';
+// import {
+//   fetchPopularAnime,
+//   fetchTopAiringAnime,
+//   fetchTopAnime,
+//   fetchTrendingAnime,
+//   fetchUpcomingSeason,
+// } from '../hooks/useApi';
+// import { CardGrid } from '../components/Cards/CardGrid';
 import { HomeCarousel } from '../components/Home/HomeCarousel';
-import { HomeSideBar } from '../components/Home/HomeSideBar';
-import { Anime } from '../hooks/animeInterface';
+// import { HomeSideBar } from '../components/Home/HomeSideBar';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+// import { Anime } from '../hooks/animeInterface';
+
+interface spotlights {
+  id: string;
+  data_id?: number;
+  poster: string;
+  title?: string;
+  japanese_title?: string;
+  description?: string;
+  tvInfo?: {
+    showType?: string;
+    duration?: string;
+    releaseDate?: string;
+    quality?: string;
+    episodeInfo?: [object];
+  };
+}
+
+interface HomeInterface {
+  success: true;
+  results: {
+    spotlights?: spotlights[];
+    trending?: [
+      {
+        id?: string;
+        data_id?: number;
+        number?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+      },
+    ];
+    topTen?: {
+      today?: {
+        id?: string;
+        data_id?: number;
+        number: string;
+        title?: string;
+        japanese_title?: string;
+        poster?: string;
+        tvInfo?: [object];
+      };
+      week?: {
+        id?: string;
+        data_id?: number;
+        number: string;
+        title?: string;
+        japanese_title?: string;
+        poster?: string;
+        tvInfo?: [object];
+      };
+      month?: {
+        id?: string;
+        data_id?: number;
+        number: string;
+        title?: string;
+        japanese_title?: string;
+        poster?: string;
+        tvInfo?: [object];
+      };
+    };
+    today?: [
+      schedule?: [
+        {
+          id?: string;
+          data_id?: number;
+          title?: string;
+          japanese_title?: string;
+          releaseDate?: string;
+          time?: string;
+          episode_no?: number;
+        },
+      ],
+    ];
+    topAiring?: [
+      {
+        id?: string;
+        data_id?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+        description?: string;
+        tvInfo?: [object];
+        adultContent?: boolean;
+      },
+    ];
+    mostPopular?: [
+      {
+        id?: string;
+        data_id?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+        description?: string;
+        tvInfo?: [object];
+        adultContent?: boolean;
+      },
+    ];
+    mostFavorite?: [
+      {
+        id?: string;
+        data_id?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+        description?: string;
+        tvInfo?: [object];
+        adultContent?: boolean;
+      },
+    ];
+    latestCompleted?: [
+      {
+        id?: string;
+        data_id?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+        description?: string;
+        tvInfo?: [object];
+        adultContent?: boolean;
+      },
+    ];
+    latestEpisode?: [
+      {
+        id?: string;
+        data_id?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+        description?: string;
+        tvInfo?: [object];
+        adultContent?: boolean;
+      },
+    ];
+    topUpcoming?: [
+      {
+        id?: string;
+        data_id?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+        description?: string;
+        tvInfo?: [object];
+        adultContent?: boolean;
+      },
+    ];
+    recentlyAdded?: [
+      {
+        id?: string;
+        data_id?: number;
+        poster?: string;
+        title?: string;
+        japanese_title?: string;
+        description?: string;
+        tvInfo?: [object];
+        adultContent?: boolean;
+      },
+    ];
+    genres?: string[];
+  };
+}
 
 const Home = () => {
-  const [activeTab, setActiveTab] = useState(() => {
-    const time = Date.now();
-    const savedData = localStorage.getItem('home tab');
-    if (savedData) {
-      const { tab, timestamp } = JSON.parse(savedData);
-      if (time - timestamp < 300000) {
-        return tab;
-      } else {
-        localStorage.removeItem('home tab');
+  // const [activeTab, setActiveTab] = useState(() => {
+  //   const time = Date.now();
+  //   const savedData = localStorage.getItem('home tab');
+  //   if (savedData) {
+  //     const { tab, timestamp } = JSON.parse(savedData);
+  //     if (time - timestamp < 300000) {
+  //       return tab;
+  //     } else {
+  //       localStorage.removeItem('home tab');
+  //     }
+  //   }
+
+  //   return 'trending';
+  // });
+
+  // useEffect(() => {
+  //   document.title = 'StreamSakura | watch anime online';
+  // }, []);
+
+  // useEffect(() => {
+  //   const time = new Date().getTime();
+  //   const tabData = JSON.stringify({ tab: activeTab, timestamp: time });
+  //   localStorage.setItem('home tab', tabData);
+  // }, [activeTab]);
+
+  // const [state, setState] = useState({
+  //   trendingAnime: [] as Anime[],
+  //   popularAnime: [] as Anime[],
+  //   topAnime: [] as Anime[],
+  //   topAiring: [] as Anime[],
+  //   Upcoming: [] as Anime[],
+  //   error: null as string | null,
+  //   loading: {
+  //     trending: true,
+  //     popular: true,
+  //     topRated: true,
+  //     topAiring: true,
+  //   },
+  // });
+
+  // useEffect(() => {
+  //   const fetchCount = 21;
+  //   const fetchData = async () => {
+  //     try {
+  //       setState((prevState) => ({ ...prevState, error: null }));
+  //       const [trending, popular, topRated, topAiring, Upcoming] =
+  //         await Promise.all([
+  //           fetchTrendingAnime(1, fetchCount),
+  //           fetchPopularAnime(1, fetchCount),
+  //           fetchTopAnime(1, fetchCount),
+  //           fetchTopAiringAnime(1, 6),
+  //           fetchUpcomingSeason(1, 6),
+  //         ]);
+  //       setState((prevState) => ({
+  //         ...prevState,
+  //         trendingAnime: trending,
+  //         popularAnime: popular,
+  //         topAnime: topRated,
+  //         topAiring: topAiring,
+  //         Upcoming: Upcoming,
+  //       }));
+  //     } catch {
+  //       setState((prevState) => ({
+  //         ...prevState,
+  //         error: 'An unexpected error occurred',
+  //       }));
+  //     } finally {
+  //       setState((prevState) => ({
+  //         ...prevState,
+  //         loading: {
+  //           trending: false,
+  //           popular: false,
+  //           topRated: false,
+  //           topAiring: false,
+  //           Upcoming: false,
+  //         },
+  //       }));
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  // const renderCardGrid = (
+  //   animeData: Anime[],
+  //   isloading: boolean,
+  //   hasError: boolean,
+  // ) => (
+  //   <div>
+  //     {isloading || hasError ? (
+  //       <div></div>
+  //     ) : (
+  //       <CardGrid
+  //         animeData={animeData}
+  //         hasNextPage={false}
+  //         onLoadMore={() => {}}
+  //       />
+  //     )}
+  //   </div>
+  // );
+
+  // const handleTabClick = (tabName: string): void => {
+  //   return setActiveTab(tabName);
+  // };
+
+  const navigate = useNavigate();
+
+  const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [homeData, setHomeData] = useState<HomeInterface | undefined>();
+
+  async function fetchHomeData(): Promise<void> {
+    try {
+      setIsLoading(true);
+      const res = await axios.get(`${BASE_URL}/api`);
+
+      if (res.status !== 200 || res.status >= 400) {
+        throw new Error(`Server Error: ${res.status}`);
+      } else if (Object.keys(res.data.results).length === 0) {
+        navigate('/404');
       }
+
+      setIsLoading(false);
+      setHomeData(res.data);
+    } catch (error) {
+      console.log(error);
     }
-
-    return 'trending';
-  });
+  }
 
   useEffect(() => {
-    document.title = 'StreamSakura | watch anime online';
+    fetchHomeData();
   }, []);
 
-  useEffect(() => {
-    const time = new Date().getTime();
-    const tabData = JSON.stringify({ tab: activeTab, timestamp: time });
-    localStorage.setItem('home tab', tabData);
-  }, [activeTab]);
-
-  const [state, setState] = useState({
-    trendingAnime: [] as Anime[],
-    popularAnime: [] as Anime[],
-    topAnime: [] as Anime[],
-    topAiring: [] as Anime[],
-    Upcoming: [] as Anime[],
-    error: null as string | null,
-    loading: {
-      trending: true,
-      popular: true,
-      topRated: true,
-      topAiring: true,
-    },
-  });
-
-  useEffect(() => {
-    const fetchCount = 21;
-    const fetchData = async () => {
-      try {
-        setState((prevState) => ({ ...prevState, error: null }));
-        const [trending, popular, topRated, topAiring, Upcoming] =
-          await Promise.all([
-            fetchTrendingAnime(1, fetchCount),
-            fetchPopularAnime(1, fetchCount),
-            fetchTopAnime(1, fetchCount),
-            fetchTopAiringAnime(1, 6),
-            fetchUpcomingSeason(1, 6),
-          ]);
-        setState((prevState) => ({
-          ...prevState,
-          trendingAnime: trending,
-          popularAnime: popular,
-          topAnime: topRated,
-          topAiring: topAiring,
-          Upcoming: Upcoming,
-        }));
-      } catch {
-        setState((prevState) => ({
-          ...prevState,
-          error: 'An unexpected error occurred',
-        }));
-      } finally {
-        setState((prevState) => ({
-          ...prevState,
-          loading: {
-            trending: false,
-            popular: false,
-            topRated: false,
-            topAiring: false,
-            Upcoming: false,
-          },
-        }));
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const renderCardGrid = (
-    animeData: Anime[],
-    isloading: boolean,
-    hasError: boolean,
-  ) => (
-    <div>
-      {isloading || hasError ? (
-        <div></div>
-      ) : (
-        <CardGrid
-          animeData={animeData}
-          hasNextPage={false}
-          onLoadMore={() => {}}
-        />
-      )}
-    </div>
-  );
-
-  const handleTabClick = (tabName: string): void => {
-    return setActiveTab(tabName);
-  };
+  const HomeCarouselData = homeData?.results?.spotlights as spotlights[];
 
   return (
     <div className='flex flex-col gap-4 mx-auto my-0'>
-      <HomeCarousel
-        data={state.trendingAnime}
-        loading={state.loading.trending}
-        error={state.error}
-      />
+      {isLoading !== true &&
+        HomeCarouselData.map((item) => (
+          <HomeCarousel
+            id={item.id}
+            title={item.title}
+            japanese_title={item.japanese_title}
+            cover={item.poster}
+            description={item.description}
+            duration={item.tvInfo?.duration}
+            type={item.tvInfo?.showType}
+            loading={isLoading}
+            // error={state}
+          />
+        ))}
       {/* <EpisodeCard /> */}
       <div className='flex gap-8 max-lg:gap-12 max-lg:flex-col'>
-        <div className='flex flex-col gap-4 min-lg:flex-4/5'>
+        {/* <div className='flex flex-col gap-4 min-lg:flex-4/5'>
           <div className='flex flex-wrap gap-2 justify-center w-full'>
             <h3
               className={` label ${activeTab === 'trending' ? 'bg-blue-300' : 'bg-gray-200 hover:bg-blue-200'}
@@ -176,9 +377,9 @@ const Home = () => {
                 !!state.error,
               )}
           </div>
-        </div>
+        </div> */}
         <div className='grid grid-cols-1 gap-10 max-lg:grid-cols-2 max-md:grid-cols-1 h-fit'>
-          <HomeSideBar
+          {/* <HomeSideBar
             animeData={state.topAiring}
             title='TOP AIRING'
             icon={<GiTv className='size-9' />}
@@ -187,7 +388,7 @@ const Home = () => {
             animeData={state.Upcoming}
             title={`UPCOMING ${getNextSeason()}`}
             icon={<GrAnnounce className='size-9' />}
-          />
+          /> */}
         </div>
       </div>
       <p>window.innerWidth={window.innerWidth}</p>
