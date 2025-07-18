4a2de1668beb547629191a6ca5a54803d2b75a52
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-07-18 01:58:16 +0100

    feat(home): add function to responsively adjust number of slides per view

--- src/components/Home/HomeVerticalSlides.tsx
+++ src/components/Home/HomeVerticalSlides.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
 // import { Anime } from '../../hooks/animeInterface';
@@ -36,14 +36,42 @@ export const HomeVerticalSlide: FC<HomeVerticalSlide> = ({
   //     !item.genres.includes('Fantasy'),
   // );
 
+  const useAdjustSlidesPerView = (): number => {
+    const [screenwidth, setScreenWidth] = useState<number>(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setScreenWidth(window.innerWidth);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
+    console.log('screenwidth >>> ', screenwidth);
+    const slidePerView =
+      screenwidth > 1250
+        ? 6
+        : screenwidth > 1000
+          ? 5
+          : screenwidth > 800
+            ? 4
+            : 3;
+
+    return slidePerView;
+  };
+
   return (
     <>
-      <div className='bg-gray-300'>
-        <h2 className='label'>trending</h2>
+      <div className='bg-gray-300 rounded-sm'>
+        <h2 className='mb-2 label'>trending</h2>
         <Swiper
-          className='relative max-w-full h-96 rounded-lg max-lg:h-80 max-sm:h-64 cursor-grab active:cursor-grabbing'
-          spaceBetween={10}
-          slidesPerView={5}
+          className='relative rounded-lg cursor-grab active:cursor-grabbing'
+          spaceBetween={5}
+          slidesPerView={useAdjustSlidesPerView()}
           // loop={true}
           // autoplay={{
           //   delay: 4000,
@@ -78,54 +106,28 @@ export const HomeVerticalSlide: FC<HomeVerticalSlide> = ({
               title={i.title || i.japanese_title}
             >
               <div className='w-full rounded-lg transition duration-200 ease-in-out scale-100 cursor-pointer group/title'>
-                <div className='transition duration-200 ease-in-out hover:-translate-y-2.5'>
-                  <div className='rounded-lg overflow-hidden relative aspect-[3/4] shadow-blue-900 animate-slideUp transition duration-200 ease-in-out shadow-[2px_2px_10px] text-left'>
-                    <div className='whfull group'>
-                      <img
-                        src={i.poster}
-                        alt=''
-                        className='whfull animation1 group-hover:brightness-60'
-                      />
-                      <TbPlayerPlayFilled
-                        className='absolute inset-0 top-1/2 left-1/2 z-10 text-3xl text-blue-300 opacity-0 transition-opacity duration-300 ease-in -translate-x-1/2 -translate-y-1/2 group-hover:opacity-100'
-                        title={'Play ' + (i.title || i.japanese_title)}
-                      />
-                    </div>
+                <div className='rounded-lg overflow-hidden relative aspect-[3/4] shadow-blue-900  transition duration-200 ease-in-out shadow-[2px_2px_10px] text-left'>
+                  <div className='whfull group'>
+                    <img
+                      src={i.poster}
+                      alt=''
+                      className='whfull group-hover:brightness-60'
+                    />
+                    <TbPlayerPlayFilled
+                      className='absolute inset-0 top-1/2 left-1/2 z-10 text-3xl text-blue-300 opacity-0 transition-opacity duration-300 ease-in -translate-x-1/2 -translate-y-1/2 group-hover:opacity-100'
+                      title={'Play ' + (i.title || i.japanese_title)}
+                    />
                   </div>
                 </div>
-                <div className='pt-1 pl-2'>
-                  <h5
-                    className='text-sm text-blue-800 font-bold transition duration-300 ease-in-out text-long group-hover/title:text-blue-500'
-                    title={'Title: ' + (i.title || i.japanese_title)}
-                  >
-                    {i.title || i.japanese_title}
-                  </h5>
-                </div>
-                <div>
-                  <div></div>
-                  <div className='flex justify-evenly'>
-                    {i.tvInfo?.showType && (
-                      <p className='px-1 text-xs bg-gray-200 text-blue-800 rounded-sm'>
-                        {i.tvInfo?.showType}
-                      </p>
-                    )}
-                    {/* {anime.releaseDate && (
-                              <p className='px-1 text-xs bg-gray-200 text-blue-800 rounded-sm'>
-                                {anime.releaseDate}
-                              </p>
-                            )}
-                            {anime.rating && (
-                              <p className='flex items-center px-1 text-xs bg-gray-200 text-blue-800 rounded-sm'>
-                                {anime.rating >= 75 ? <TbStarFilled /> : <TbStarHalfFilled />}
-                                {anime.rating}
-                              </p>
-                            )} */}
-                  </div>
-                </div>
+                <h5
+                  className='text-sm font-bold text-center text-blue-800 transition duration-300 ease-in-out text-long group-hover/title:text-blue-500'
+                  title={'Title: ' + (i.title || i.japanese_title)}
+                >
+                  {i.title || i.japanese_title}
+                </h5>
               </div>
             </SwiperSlide>
           ))}
-
           <div className='swiper-pagination'></div>
         </Swiper>
       </div>
