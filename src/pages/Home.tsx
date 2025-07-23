8047c644d40e78afa498e74ea3c63f6ba983e6b8
Farhang-Osman <168280475+Farhang-Osman@users.noreply.github.com>
2025-07-23 20:03:38 +0100

    feat(home): add status to tabs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -77,117 +77,35 @@ interface HomeInterface {
 }
 
 const Home = () => {
-  // const [activeTab, setActiveTab] = useState(() => {
-  //   const time = Date.now();
-  //   const savedData = localStorage.getItem('home tab');
-  //   if (savedData) {
-  //     const { tab, timestamp } = JSON.parse(savedData);
-  //     if (time - timestamp < 300000) {
-  //       return tab;
-  //     } else {
-  //       localStorage.removeItem('home tab');
-  //     }
-  //   }
-
-  //   return 'trending';
-  // });
-
   // useEffect(() => {
   //   document.title = 'StreamSakura | watch anime online';
   // }, []);
-
-  // useEffect(() => {
-  //   const time = new Date().getTime();
-  //   const tabData = JSON.stringify({ tab: activeTab, timestamp: time });
-  //   localStorage.setItem('home tab', tabData);
-  // }, [activeTab]);
-
-  // const [state, setState] = useState({
-  //   trendingAnime: [] as Anime[],
-  //   popularAnime: [] as Anime[],
-  //   topAnime: [] as Anime[],
-  //   topAiring: [] as Anime[],
-  //   Upcoming: [] as Anime[],
-  //   error: null as string | null,
-  //   loading: {
-  //     trending: true,
-  //     popular: true,
-  //     topRated: true,
-  //     topAiring: true,
-  //   },
-  // });
-
-  // useEffect(() => {
-  //   const fetchCount = 21;
-  //   const fetchData = async () => {
-  //     try {
-  //       setState((prevState) => ({ ...prevState, error: null }));
-  //       const [trending, popular, topRated, topAiring, Upcoming] =
-  //         await Promise.all([
-  //           fetchTrendingAnime(1, fetchCount),
-  //           fetchPopularAnime(1, fetchCount),
-  //           fetchTopAnime(1, fetchCount),
-  //           fetchTopAiringAnime(1, 6),
-  //           fetchUpcomingSeason(1, 6),
-  //         ]);
-  //       setState((prevState) => ({
-  //         ...prevState,
-  //         trendingAnime: trending,
-  //         popularAnime: popular,
-  //         topAnime: topRated,
-  //         topAiring: topAiring,
-  //         Upcoming: Upcoming,
-  //       }));
-  //     } catch {
-  //       setState((prevState) => ({
-  //         ...prevState,
-  //         error: 'An unexpected error occurred',
-  //       }));
-  //     } finally {
-  //       setState((prevState) => ({
-  //         ...prevState,
-  //         loading: {
-  //           trending: false,
-  //           popular: false,
-  //           topRated: false,
-  //           topAiring: false,
-  //           Upcoming: false,
-  //         },
-  //       }));
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
-  // const renderCardGrid = (
-  //   animeData: Anime[],
-  //   isloading: boolean,
-  //   hasError: boolean,
-  // ) => (
-  //   <div>
-  //     {isloading || hasError ? (
-  //       <div></div>
-  //     ) : (
-  //       <CardGrid
-  //         animeData={animeData}
-  //         hasNextPage={false}
-  //         onLoadMore={() => {}}
-  //       />
-  //     )}
-  //   </div>
-  // );
-
-  // const handleTabClick = (tabName: string): void => {
-  //   return setActiveTab(tabName);
-  // };
 
   const navigate = useNavigate();
 
   const BASE_URL = import.meta.env.VITE_BACKEND_URL;
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [homeData, setHomeData] = useState<HomeInterface | undefined>();
-  const [activeTab, setActiveTab] = useState<string>();
+  const [activeTab, setActiveTab] = useState((): string => {
+    const time = Date.now();
+    const savedData = localStorage.getItem('home tab');
+    if (savedData) {
+      const { tab, timestamp } = JSON.parse(savedData);
+      if (time - timestamp < 300000) {
+        return tab;
+      } else {
+        localStorage.removeItem('home tab');
+      }
+    }
+
+    return 'topUpcoming';
+  });
+
+  useEffect(() => {
+    const time = new Date().getTime();
+    const tabData = JSON.stringify({ tab: activeTab, timestamp: time });
+    localStorage.setItem('home tab', tabData);
+  }, [activeTab]);
 
   async function fetchHomeData(): Promise<void> {
     try {
@@ -244,24 +162,24 @@ const Home = () => {
             <h3
               className={` label ${activeTab === 'topUpcoming' ? 'bg-blue-300' : 'bg-gray-200 hover:bg-blue-200'}
              transition-colors duration-100 ease-in-out cursor-pointer`}
-              title='topUpcoming Tab'
+              title='Top Upcoming Tab'
               onClick={() => setActiveTab('topUpcoming')}
             >
               Top Upcoming
             </h3>
             <div
-              className={`${activeTab === 'MostPopular' ? 'bg-blue-300' : 'bg-gray-200 hover:bg-blue-200'}
+              className={`${activeTab === 'mostPopular' ? 'bg-blue-300' : 'bg-gray-200 hover:bg-blue-200'}
             label transition-colors duration-100 ease-in-out cursor-pointer`}
-              title='Popular Tab'
-              onClick={() => setActiveTab('MostPopular')}
+              title='Most Popular Tab'
+              onClick={() => setActiveTab('mostPopular')}
             >
               Most Popular
             </div>
             <div
-              className={`${activeTab === 'MostFavorite' ? 'bg-blue-300' : 'bg-gray-200 hover:bg-blue-200'}
+              className={`${activeTab === 'mostFavorite' ? 'bg-blue-300' : 'bg-gray-200 hover:bg-blue-200'}
             label transition-colors duration-100 ease-in-out cursor-pointer`}
               title='Most Favorite Tab'
-              onClick={() => setActiveTab('MostFavorite')}
+              onClick={() => setActiveTab('mostFavorite')}
             >
               Most Favorite
             </div>
@@ -269,9 +187,9 @@ const Home = () => {
           <div>
             {activeTab === 'topUpcoming' &&
               renderCardGrid(homeData?.results?.topUpcoming as HomeAnimeInfo[])}
-            {activeTab === 'MostPopular' &&
+            {activeTab === 'mostPopular' &&
               renderCardGrid(homeData?.results?.mostPopular as HomeAnimeInfo[])}
-            {activeTab === 'MostFavorite' &&
+            {activeTab === 'mostFavorite' &&
               renderCardGrid(
                 homeData?.results?.mostFavorite as HomeAnimeInfo[],
               )}
